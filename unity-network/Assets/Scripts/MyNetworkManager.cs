d70fdd87ef20dc43671dfbdd1dc8e161021e7880
Alexandre Demeester <alexandre.demeester@epitech.eu>
2022-01-20 19:14:25 +0100

    feat: listing players on lobby

--- unity-network/Assets/Scripts/MyNetworkManager.cs
+++ unity-network/Assets/Scripts/MyNetworkManager.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using Mirror;
+using TMPro;
 using UnityEngine;
 
 public class MyNetworkManager : NetworkManager
 {
+    [SerializeField] private Transform PlayerListView = null;
+    public GameObject PlayerTagPrefab = null;
+
     // Update is called once per frame
     void Update()
     {
@@ -13,9 +17,25 @@ public class MyNetworkManager : NetworkManager
 
     public override void OnServerAddPlayer(NetworkConnection conn)
     {
-        //base.OnServerAddPlayer(conn);
-
+        base.OnServerAddPlayer(conn);
         Debug.Log($"On add Player for lobby addr: {conn.address}");
+        GameObject playerTag = Instantiate(PlayerTagPrefab, PlayerListView);
+        NetworkServer.Spawn(playerTag, conn);
+        playerTag.GetComponentInChildren<TMP_Text>().text = $"player : {conn.address}";
+    }
+
+    public override void OnServerConnect(NetworkConnection conn)
+    {
+        //base.OnServerConnect(conn);
+        Debug.Log($"On Connect to server : {conn.address}");
+        //conn.
+    }
+
+    public override void OnClientConnect()
+    {
+        base.OnClientConnect();
+        Debug.Log($"On Connect to server :");
+
     }
 
 }

f718615a99aebef6eb6bd2fc159dc3a5d4c26975
Rokuo <41430347+Rokuo@users.noreply.github.com>
2022-01-22 04:18:15 +0900

    Merge/lobby gameplay (#2)
    * feat: Lobby scene
    
    * feat: Lobby scene created
    
    * feat: TMP + Jua font
    
    * feat: mirror hud joining lobbies
    
    * update gitignore
    
    * reverse gitignore
    
    * update gitignore
    
    * add player movement + dash + jump
    
    * merge: fix conflict in unity scene
    
    * merge: successfuly linked gameplay & lobby scene with mirror
    
    * feat: started server and client logic (CmdMove: Ok)
    
    Co-authored-by: arthurqqq <arthur.malarde@epitech.eu>

---
+++ unity-network/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,140 @@
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+
+public class PlayerMove : NetworkBehaviour
+{
+    private Rigidbody2D rb;
+    [SerializeField] private int speed;
+    private Vector3 moveDirection;
+
+    private bool isDashing = false;
+    private bool isDashAvailable = true;
+    IEnumerator dashCoroutine;
+
+    [SerializeField] private float jumpVelocity;
+    [SerializeField] private float jumpRatio;
+    [SerializeField] private float jumpPeakTiming;
+    private bool canJump = true;
+
+    float horizontalDirection = 1;
+    float verticalDirection = 1;
+    [SerializeField] private float normalGravity;
+
+    float horizontal;
+    float vertical;
+
+    private float moveX = 0f;
+    private float moveY = 0f;
+
+    #region Server
+
+    [Command]
+    private void CmdJump()
+    {
+        canJump = false;
+        jumpVelocity = 30f;
+        Invoke("ResetJumpVelocity", jumpPeakTiming);
+        Debug.Log("jump");
+        rb.velocity = new Vector2(0f, jumpVelocity);
+    }
+
+    [Command]
+    private void CmdMove(float horizontal, int speed, bool isDashing, float jumpVelocity)
+    {
+        rb.velocity = new Vector2(horizontal * speed, isDashing ? 0 : jumpVelocity);
+    }
+
+    #endregion
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        normalGravity = rb.gravityScale;
+    }
+
+    private void OnCollisionStay2D(Collision2D other) {
+        if (other.collider.CompareTag("Wall")) {
+            canJump = true;
+            Debug.Log("collision");
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other) {
+        if (other.collider.CompareTag("Wall")) {
+            canJump = false;
+            Debug.Log("collision");
+        }
+    }
+
+    void Update()
+    {
+        if (!isLocalPlayer || !hasAuthority)
+            return;
+
+        if (horizontal != 0)
+            horizontalDirection = horizontal;
+        if (vertical != 0)
+            verticalDirection = vertical;
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+        if (Input.GetKeyDown(KeyCode.Space) && canJump) {
+            //CmdJump();
+            canJump = false;
+            jumpVelocity = 30f;
+            Invoke("ResetJumpVelocity", jumpPeakTiming);
+            Debug.Log("jump");
+            rb.velocity = new Vector2(0f, jumpVelocity);
+        }
+
+        if (Input.GetMouseButtonDown(1) && isDashAvailable) {
+            if (dashCoroutine != null)
+                StopCoroutine(dashCoroutine);
+            dashCoroutine = Dash(0.1f, 2);
+            StartCoroutine(dashCoroutine);
+        }
+
+        // vertical = Input.GetAxis("Jump");
+        // ProcessInputs();
+    }
+
+    void ResetJumpVelocity() {
+        StartCoroutine(SlowJump());
+    }
+
+    IEnumerator SlowJump() {
+        while (jumpVelocity >= 0) {
+            jumpVelocity -= jumpRatio * Time.deltaTime;
+            yield return null;
+        }
+        jumpVelocity = 0f;
+        yield break;
+    }
+
+    IEnumerator Dash(float dashDuration, float dashCooldown) {
+        isDashing = true;
+        isDashAvailable = false;
+        rb.gravityScale = 0f;
+        rb.velocity = Vector2.zero;
+        yield return new WaitForSeconds(dashDuration);
+        isDashing = false;
+        rb.gravityScale = normalGravity;
+        rb.velocity = Vector2.zero;
+        yield return new WaitForSeconds(dashCooldown);
+        isDashAvailable = true;
+    }
+
+    void FixedUpdate() {
+        if (!isLocalPlayer || !hasAuthority)
+            return;
+
+        //rb.velocity = new Vector2(horizontal * speed, isDashing ? 0 : jumpVelocity);
+        CmdMove(horizontal, speed, isDashing, jumpVelocity);
+        //if (isDashing == true) {
+        //    rb.AddForce(new Vector2(horizontalDirection * 25, 0), ForceMode2D.Impulse);
+        //}
+    }
+
+}
